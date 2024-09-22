162c02da8a3771d2a0c0b7ff31f78d59b9ff3f7c
Charles <charliecharleston1121@gmail.com>
2024-09-22 20:51:38 +0100

    demo Heder is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Demo from "./components/Demo/Demo";
+import DemoHeader from "./components/Demo/DemoHeader";
+import HomeHeader from "./components/Home/HomeHeader";
 
+function App() {
+  const auth = false;
   return (
+    //<div className="bg-[url('src/components/Demo/bg_img.jpeg')]">
     <>
-      <h1 className='text-red-500'>Hello world</h1>
+      {auth ? <HomeHeader /> : <DemoHeader />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
     </>
-  )
+    //</div>
+  );
 }
 
 export default App
---
+++ src/components/Demo/DemoHeader.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { nav } from '../../data'
+const DemoHeader = () => {
+  return (
+    <header className="border-b border-black sticky top-0 z-50 bg-transparent">
+      <div className="size h-[70px] flex items-center justify-between">
+        <Link to={"/"}>
+          <img
+            className='h-[1.5rem]'
+            src="src\components\Demo\Medium_(2).svg"
+            alt="logo" />
+        </Link>
+        <div className='flex items-center gap-5'>
+          <div className='hidden text-sm sm:flex items-center gap-5'>
+            {nav.map((link, i) => (
+              <Link key={i} to={link.path}>{link.title}</Link>
+            ))}
+          </div>
+          <div className="relative">
+            <button className='hidden text-sm sm:flex items-center gap-5'>Sign In</button>
+
+          </div>
+          <button className='bg-black text-white rounded-full px3 p-2 text-sm font-medium'>
+            Get Started
+          </button>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default DemoHeader
